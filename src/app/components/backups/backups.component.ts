7b5f450def31b5f3ac6e6ffd8dfa7e5d3bdfdba5
kaoutar derouach <derouachkaoutar@gmail.com>
2023-10-31 14:52:07 +0100

    6th commit

--- src/app/components/backups/backups.component.ts
+++ src/app/components/backups/backups.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable,Subscription, interval  } from 'rxjs';
+import { BackupsService } from 'src/app/services/backups.service';
 @Component({
   selector: 'app-backups',
   templateUrl: './backups.component.html',
   styleUrls: ['./backups.component.css']
 })
-export class BackupsComponent {
+export class BackupsComponent implements OnInit{
+  private updateSubscription!: Subscription;
+  backups =  {} as any;
+  resultbackups: any[] = [];
+  logs={} as any;
+  resultLogs:any[] = [];
+  id:any;
+  gh!: string;
+  ngOnInit(): void {
+    this.updateSubscription = interval(300).subscribe((val)=>{
+      this.id = this.route.snapshot.paramMap.get('id');
+      this.getBackup(this.id);
+      this.getLog();
+
+
+    });
+
+   // this.getbackups();
+   }
+   constructor(private backupservice:BackupsService, private route: ActivatedRoute){
+
+   }
+
+   getBackup(type:any){
+    this.backupservice.find(type)
+         .subscribe(backups => {this.resultbackups = this.backups = backups})
+         console.log("------------------backups------------",this.resultbackups);
+
+
+
+   }
+   getLog(){
+    this.backupservice.findLog()
+         .subscribe(logs => {this.resultLogs = this.logs = logs})
+   }
+   searchText: string='';
+   onSearchTextEntered(searchValue: string){
+    this.searchText = searchValue;
+    console.log(this.searchText);
+
+   }
 
 }
