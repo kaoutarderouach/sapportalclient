47da7593fbf17f685837ada20e13a65527ef4b3e
kaoutar derouach <derouachkaoutar@gmail.com>
2023-10-31 15:15:51 +0100

    commit

--- src/app/components/filter-type/filter-type.component.ts
+++ src/app/components/filter-type/filter-type.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filter-type',
   templateUrl: './filter-type.component.html',
   styleUrls: ['./filter-type.component.css']
 })
-export class FilterTypeComponent {
+export class FilterTypeComponent implements OnInit{
+  constructor(){}
+  ngOnInit(): void {
+
+  }
+  @Input('total') all: number = 0;
+  @Input() free: number = 0;
+  @Input() premium: number = 0;
+
+  selectedTypeButtonValue: string = 'All';
+
+  @Output()
+  filterTypeButtonSelectedChanged: EventEmitter<string> = new EventEmitter<string>();
+  onTypeButtonSelectedChanged(){
+    this.filterTypeButtonSelectedChanged.emit(this.selectedTypeButtonValue);
+    if(this.selectedTypeButtonValue==="log"){
+      console.log(this.selectedTypeButtonValue);
+    }
+
+
+  }
+
+
 
 }
