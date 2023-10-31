3fdeafc07d9e42b09b24bd98fae8d8e76ee9031e
kaoutar derouach <derouachkaoutar@gmail.com>
2023-10-31 14:48:09 +0100

    5th commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CompteRendusService } from './services/compte-rendus.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'portalsap';
+
+export class AppComponent implements OnInit {
+  ss:string=""
+  tasks =  {} as any;
+  resultTasks: any[] = [];
+  constructor(private compteRendusService:CompteRendusService){}
+  getEp1(){
+    this.compteRendusService.findAll()
+         .subscribe(tasks => {this.resultTasks = this.tasks = tasks})
+
+   }
+  ngOnInit(): void {
+    this.getEp1()
+
+     this.ss=Array.from('some string')[0];
+
+  }
+  title = 'ClientApp';
+  startDate: string="";
+  endDate: string="";
+  logged=0
+  message:any
+
+  receiveMessage($event: any){
+    this.message=$event
+    this.logged=1
+    this.sendMessage();
+
+  }
+  @Output() event = new EventEmitter<string>()
+  sendMessage(){
+    this.event.emit(this.message.name)
+  }
+
+
+
 }
--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UrlConnectionService } from 'src/app/services/url-connection.service';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AdminDashboardComponent {
 
+  name: string ="";
+  username: string ="";
+  password: string ="";
+  role=0;
+
+  constructor(private http:HttpClient, private urlConnectionervice:UrlConnectionService ){}
+  urlAdmin=this.urlConnectionervice.getAdminUrl()
+
+  onSubmit(){
+    //this.http.post('http://localhost:3000/api/register', {
+      this.http.post(this.urlAdmin+'api/register', {
+      name: this.name,
+      username: this.username,
+      password: this.password,
+      role:this.role,
+    }).subscribe((data) => {
+      console.log(data);
+
+
+
+    });
+
+
+
+  }
 }
